574beb5aec43d54316200a33ed4713d6ac301cb2
Md Zahid Raza <zahid7292@ymail.com>
2017-07-22 01:26:29 +0530

    release: 2.1.0 (stable)

--- web-app/src/js/index.js
+++ web-app/src/js/index.js
@@ -31,6 +31,3 @@ ReactDOM.render((
 
 document.body.classList.remove('loading');
 
-http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm
-
-http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm
